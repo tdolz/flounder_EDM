03067ec7ec6e2b3ea9c6cf211bef95a90de3bff5
Tara Dolan <taradolan@Taras-MacBook-Pro.local>
2024-06-06 11:45:36 -0400

    hope this works. large commit

---
+++ Vinny_Data.R
@@ -0,0 +1,13 @@
+#Exploring the time series 5/14/24
+
+#install.packages("Hmisc")
+library(Hmisc)
+library(mdbtools)
+library(tidyverse)
+library(GPEDM)
+
+
+###Load access database###
+
+vin <- mdb.get(file="Beam_Trawl_DBASE.mdb")
+
